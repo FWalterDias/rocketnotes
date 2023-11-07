8ba60760ca187f4ddf2446b0ccf8c16208597f35
Francisco Walter <franciscowalterd@gmail.com>
2023-11-07 01:01:35 -0300

    adicionando global style

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,13 @@ import './index.css';
 import { MyRouts } from './routes.jsx';
 import { ThemeProvider } from 'styled-components';
 import theme  from "./styles/theme.js";
+import GlobalStyle from "./styles/global.js";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter >
       <ThemeProvider theme={theme}>
+        <GlobalStyle />
         <MyRouts />
       </ThemeProvider>
     </BrowserRouter>
